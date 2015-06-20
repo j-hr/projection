1bb44f94f58f41b4db3da4176c269f9de76b2725
j-hr <hrncir.jakub@gmail.com>
2015-06-20 23:11:10 +0200

    Womersley solution

--- pipe_test.py
+++ pipe_test.py
@@ -3,6 +3,8 @@ from dolfin import *
 import math #TODO add "math."
 import csv
 import sys
+from sympy import I, re, sqrt, exp, symbols, lambdify, besselj
+from scipy.special import jv 
 
 #TODO if method = ... >> if hasTentative
 
@@ -49,15 +51,23 @@ R=5.0 # cylinder radius
 #==Boundary Conditions================================================================================
 # boundary parts: 1 walls, 2 inflow, 3 outflow
 noslip  = Constant((0.0, 0.0, 0.0))
-# Womersley profile (1 s period)
-#TODO pulse0 - u(0)=0
 #TODO pulsePrec - u(0) from precomputed solution (steady Stokes problem)
 if str_type == "steady" :
     v_in = Expression(("0.0","0.0","(t<0.5)?((sin(pi*t))*(1081.48-43.2592*(x[0]*x[0]+x[1]*x[1]))):(1081.48-43.2592*(x[0]*x[0]+x[1]*x[1]))"),t=0)
-elif str_type == "pulse0" :
-    v_in = Expression(("0.0","0.0","(t<0.5)?((sin(pi*t))*(1081.48-43.2592*(x[0]*x[0]+x[1]*x[1]))):(1081.48-43.2592*(x[0]*x[0]+x[1]*x[1]))"),t=0)
-elif str_type == "pulsePrec" :
-    v_in = Expression(("0.0","0.0","(t<0.5)?((sin(pi*t))*(1081.48-43.2592*(x[0]*x[0]+x[1]*x[1]))):(1081.48-43.2592*(x[0]*x[0]+x[1]*x[1]))"),t=0)
+elif str_type == "pulse0" or  str_type == "pulsePrec" :      
+    r,t = symbols('r t')
+    u = -43.2592*r**2 + (-11.799 + 0.60076*I)*((0.000735686 - 0.000528035*I)*besselj(0, r*(1.84042 + 1.84042*I)) + 1)*exp(-8*I*pi*t) + (-11.799 - 0.60076*I)*((0.000735686 + 0.000528035*I)*besselj(0, r*(1.84042 - 1.84042*I)) + 1)*exp(8*I*pi*t) + (-26.3758 - 4.65265*I)*(-(0.000814244 - 0.00277126*I)*besselj(0, r*(1.59385 - 1.59385*I)) + 1)*exp(6*I*pi*t) + (-26.3758 + 4.65265*I)*(-(0.000814244 + 0.00277126*I)*besselj(0, r*(1.59385 + 1.59385*I)) + 1)*exp(-6*I*pi*t) + (-51.6771 + 27.3133*I)*(-(0.0110653 - 0.00200668*I)*besselj(0, r*(1.30138 + 1.30138*I)) + 1)*exp(-4*I*pi*t) + (-51.6771 - 27.3133*I)*(-(0.0110653 + 0.00200668*I)*besselj(0, r*(1.30138 - 1.30138*I)) + 1)*exp(4*I*pi*t) + (-33.1594 - 95.2423*I)*((0.0314408 - 0.0549981*I)*besselj(0, r*(0.920212 - 0.920212*I)) + 1)*exp(2*I*pi*t) + (-33.1594 + 95.2423*I)*((0.0314408 + 0.0549981*I)*besselj(0, r*(0.920212 + 0.920212*I)) + 1)*exp(-2*I*pi*t) + 1081.48
+    # how this works?
+    u_lambda = lambdify([r,t], u, ['numpy', {'besselj': jv}]) 
+    class WomersleyProfile(Expression):
+        def eval(self, value, x):
+            rad = float(sqrt(x[0]*x[0]+x[1]*x[1])) # conversion to float needed, u_lambda (and near) cannot use sympy Float as input
+            value[0] = 0
+            value[1] = 0
+            value[2] = 0 if near(rad,R) else re(u_lambda(rad,t)) # do not evaluate on boundaries, it's 0
+        def value_shape(self):
+            return (3,)
+    v_in=WomersleyProfile()
 
 #==Output settings====================================================================================
 # Create files for storing solution
@@ -73,7 +83,7 @@ if str_method=="chorinExpl" :
 if str_type == "steady" :
     solution = Expression(("0.0","0.0","(1081.48-43.2592*(x[0]*x[0]+x[1]*x[1]))"))
 elif (str_type == "pulse0") or (str_type == "pulsePrec") :
-    solution = Expression(("0.0","0.0","(1081.48-43.2592*(x[0]*x[0]+x[1]*x[1]))")) #TODO
+    solution = WomersleyProfile()
 
 
 #==Error control====================================================================================
