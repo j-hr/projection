02a2d2eea48d2798962e537389c4d3d57f5a4e96
j-hr <hrncir.jakub@gmail.com>
2016-01-15 01:14:43 +0100

    Generating timelines plots.

--- generate_plots.py
+++ generate_plots.py
@@ -26,7 +26,7 @@ for item in header:
     if str(item).startswith('last_cycle_') and (str(item).endswith('r') or str(item).endswith('n')):
         characteristics.append(item[11:])
         ch_index[item[11:]] = header.index(item)
-print('Used characteristics:', characteristics)
+print('Loaded characteristics:', characteristics)
 problems = {}
 for line in csvreader:
     # collect names of problems (first five chars of parameter "name")
@@ -48,6 +48,7 @@ formats3 = ['x:', '+:', '1:']
 formats5 = ['1:', '2:', '.:', 'x:', '+:']
 dts = {1: 100, 2: 50, 3: 10, 4: 5, 5: 1}
 # create convergence plots =========================================================================
+# default characteristics: ['CE_L2r', 'CE_H1r', 'CE_H1wr', 'PEn', 'TE_L2r', 'TE_H1r', 'TE_H1wr', 'PTEn', 'FEr']
 for ch in characteristics:
     # CHARACTERISTIC/DT
     for (f, fs) in factors.iteritems():
@@ -119,104 +120,216 @@ for ch in characteristics:
             plt.savefig('plots/C_' + ch + '_f%d' % f + '_CS.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
         plt.close()
 
-exit()
-
-# reports over seconds =========================================================================
+# reports over seconds - load data =========================================================================
+characteristics_seconds = []
 csvfile = open('done_merged_seconds.csv', 'r')
 csvreader = csv.reader(csvfile, delimiter=';', escapechar='|')
 seconds_list = next(csvreader)
 l = len(seconds_list)
-seconds_list = [int(x) for x in seconds_list[4:l]]
+seconds_list = [int(x) for x in seconds_list[3:l]]
 print(seconds_list)
-data_list = []
-data_types = {}
 for line in csvreader:
-    newdata = [line[0]]+line[2:4]
-    for val in line[4:l]:
-        newdata.append(float(val))
-    data_list.append(newdata)
-    print(line[3])
-    if not line[3] in data_types:
-        data_types[line[3]] = [1, line[2]]
+    data_place = problems[line[0][0:5]][line[0]]
+    data_list = [float(x) for x in line[3:l]]
+    if 'seconds' in data_place:
+        data_place['seconds'][line[2]] = data_list
     else:
-        data_types[line[3]][0] += 1
+        data_place['seconds'] = {line[2]: data_list}
+    if not line[2] in characteristics_seconds:
+        characteristics_seconds.append(line[2])
 csvfile.close()
-print(data_types)
-print(data_list)
-for type_key in data_types.iterkeys():
-    print(type_key, data_types[type_key])
-    max_value = 0
-    empty_plot = True
-    colors = plt.get_cmap('jet')(np.linspace(0, 1.0, data_types[type_key][0]))
-    i = 0
-    # TODO use less collors using different line types (write function of N to create list of formatstrings and colors)
-    # TODO colors should respect problem configurations
-    for data in data_list:
-        if data[2] == type_key and data[3:]:
-            # print(data[3:])
-            newmax = max(data[4:])  # first value is not counted for maximum (it is too high)
-            if newmax > max_value:
-                max_value = newmax
-            plt.plot(seconds_list, data[3:], '.-', label=data[0], color=colors[i])
-            empty_plot = False
-            i += 1
-    if not empty_plot:
-        # print(max_value)
-        plt.xlabel('cycles')
-        plt.title(data_types[type_key][1])
-        plt.xticks(seconds_list)
-        lgd = plt.legend(bbox_to_anchor=(1.4, 1.0))
-        plt.axis([min(seconds_list), max(seconds_list), 0, max_value])
-        plt.savefig('plots/S_' + type_key + '.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
-    plt.close()
+print('Characteristics for second-lines:', characteristics_seconds)
 
-# time lines =========================================================================
-exit()
+# reports over seconds - generate plots =========================================================================
+
+# QQ what to plot?
+
+# for type_key in data_types.iterkeys():
+#     print(type_key, data_types[type_key])
+#     max_value = 0
+#     empty_plot = True
+#     colors = plt.get_cmap('jet')(np.linspace(0, 1.0, data_types[type_key][0]))
+#     i = 0
+#     # TODO use less collors using different line types (write function of N to create list of formatstrings and colors)
+#     # TODO colors should respect problem configurations
+#     for data in data_list:
+#         if data[2] == type_key and data[3:]:
+#             # print(data[3:])
+#             newmax = max(data[4:])  # first value is not counted for maximum (it is too high)
+#             if newmax > max_value:
+#                 max_value = newmax
+#             plt.plot(seconds_list, data[3:], '.-', label=data[0], color=colors[i])
+#             empty_plot = False
+#             i += 1
+#     if not empty_plot:
+#         # print(max_value)
+#         plt.xlabel('cycles')
+#         plt.title(data_types[type_key][1])
+#         plt.xticks(seconds_list)
+#         lgd = plt.legend(bbox_to_anchor=(1.4, 1.0))
+#         plt.axis([min(seconds_list), max(seconds_list), 0, max_value])
+#         plt.savefig('plots/S_' + type_key + '.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
+#     plt.close()
+
+# time lines - load data =========================================================================
 # TODO different timesteps? introduce timestep value into .csv
-csvfile = open('done_merged_time_lines.csv', 'r')
-csvreader = csv.reader(csvfile, delimiter=';', escapechar='|')
-time_list = next(csvreader)
-l = len(time_list)
-time_list = [float(x) for x in time_list[3:l]]
-print(time_list)
-data_list = []
-data_types = {}
-for line in csvreader:
-    newdata = [line[0]]+line[2:4]
-    for val in line[4:l]:
-        newdata.append(float(val))
-    data_list.append(newdata)
-    if not line[3] in data_types:
-        data_types[line[3]] = [1, line[2]]
-    else:
-        data_types[line[3]][0] += 1
-csvfile.close()
-print(data_types)
-# TODO insert analytic values into right plots ()
-for type_key in data_types.iterkeys():
-    print(type_key, data_types[type_key])
-    max_value = 0
-    min_value = 1e16
-    empty_plot = True
-    colors = plt.get_cmap('jet')(np.linspace(0, 1.0, data_types[type_key][0]))
-    for data in data_list:
-        if data[2] == type_key and data[3:]:
-            # print(data[3:])
-            newmax = max(data[3:])
-            newmin = min(data[3:])
-            if newmax > max_value:
-                max_value = newmax
-            if newmin < max_value:
-                min_value = newmin
-            plt.plot(time_list, data[3:], '.-', label=data[0])
-            empty_plot = False
-    if not empty_plot:
-        # print(max_value)
-        plt.xlabel('cycles')
-        plt.title(data_types[type_key][1])
-        # plt.xticks(seconds_list)
-        plt.legend(bbox_to_anchor=(1.4, 1.0))
-        plt.axis([min(time_list), max(time_list), min_value, max_value])
-        plt.savefig('plots/TL_' + type_key + '.png')
-    plt.close()
+characteristics_timelines = []
+times = {}
+for i in range(1, 6):
+    csvfile = open('done_merged_time_lines%d.csv' % i, 'r')
+    csvreader = csv.reader(csvfile, delimiter=';', escapechar='|')
+    line = next(csvreader)
+    times[i] = [float(x) for x in line[3:]]
+    print(times[i])
+    for line in csvreader:
+        data_place = problems[line[0][0:5]][line[0]]
+        data_list = [float(x) for x in line[3:]]
+        if 'timelines' in data_place:
+            data_place['timelines'][line[2]] = data_list
+        else:
+            data_place['timelines'] = {line[2]: data_list}
+        if not line[2] in characteristics_timelines:
+            characteristics_timelines.append(line[2])
+    csvfile.close()
+print('Characteristics for time-lines:', characteristics_timelines)
+# last time characteristics:
+# 'AVN_L2', 'AVN_H1',
+# 'CE_L2', 'CE_L2s', 'CE_L2n', 'CE_L2r',
+# 'DT', 'DTs',
+# 'APG', 'APGs', 'APGn',
+# 'PGEA', 'PGEAs', 'PGEAn',
+# 'CE_H1', 'CE_H1s', 'CE_H1r',
+# 'PG', 'PGs', 'PGn',
+# 'AF',
+# 'CE_H1w', 'CE_H1ws', 'CE_H1wr',
+# 'APN',
+# 'TE_L2', 'TE_L2s', 'TE_L2n', 'TE_L2r',
+# 'TE_H1', 'TE_H1s', 'TE_H1r',
+# 'DC', 'DCs',
+# 'TE_H1w', 'TE_H1ws', 'TE_H1wr',
+# 'PE', 'PEs', 'PEn',
+# 'PGE', 'PGEs', 'PGEn',
+# 'FE', 'FEr',
+# 'AVN_H1w'
 
+# define plots:
+
+# plot: one parameter, one problem, one factor, 3 meshes, 5 dts
+plot1 = ['CE_L2', 'CE_L2r', 'CE_H1', 'CE_H1r', 'CE_H1w', 'CE_H1wr', 'TE_L2', 'TE_L2r', 'TE_H1', 'TE_H1t', 'TE_H1w',
+         'TE_H1wr', 'FE', 'FEr', 'PE', 'PEn', 'PGE', 'PGEn',]
+
+# QQ which to use?  >> let choose shorter set
+
+# same plot with analytic value
+plot2 = {'PG': 'APG'}  # NT: iplement ad hoc
+
+# QQ other plots?
+
+formats3 = ['-', '--', ':']
+dtToSteps = {1: 10, 2: 20, 3: 100, 4: 200, 5: 1000}  # time steps per second
+
+for ch in plot1:
+    if ch in characteristics_timelines:
+        for (f, fs) in factors.iteritems():
+            for problem in problems.iterkeys():
+                plot_empty = True
+                colors = plt.get_cmap('jet')(np.linspace(0, 1.0, 5))
+                max_ = 0
+                min_ = 1e16
+                max_lc = 0
+                min_lc = 1e16
+                for i in meshes:
+                    for (t, dt_ms) in dts.iteritems():
+                        name = problem + ('%d%d%d' % (f, i, t))
+                        x = []
+                        y = []
+                        if name in problems[problem]:
+                            d = problems[problem][name]
+                            y = d['timelines'][ch]
+                            x = times[t]
+                            newmin = min(y[dtToSteps[t]:])
+                            if newmin < min_:
+                                min_ = newmin
+                            newmax = max(y[dtToSteps[t]:])
+                            if newmax > max_:
+                                max_ = newmax
+                            newmin = min(y[(5*dtToSteps[t]):])  # NT programmed only for 6 cycles!
+                            if newmin < min_lc:
+                                min_lc = newmin
+                            newmax = max(y[(5*dtToSteps[t]):])  # NT programmed only for 6 cycles!
+                            if newmax > max_lc:
+                                max_lc = newmax
+                        if y:
+                            plot_empty = False
+                            print(ch, ' on mesh ', i)
+                            plt.plot(x, y, formats3[i-1], label=problem + (' on mesh %d' % i) + 'with dt=%d ms' % dt_ms,
+                                     color=colors[t-1])
+                if not plot_empty:
+                    plt.xlabel('time')
+                    axis = [0, 6, min_, max_]  # NT programmed only for 6 cycles!
+                    plt.axis(axis)
+                    plt.title(ch + ' for ' + problem + ' for factor=' + fs)
+                    lgd = plt.legend(bbox_to_anchor=(1.7, 1.0))
+                    plt.savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + '.png', bbox_extra_artists=(lgd,),
+                                bbox_inches='tight', dpi=300)
+                    # save same plot only for last cycle
+                    axis = [5, 6, min_lc, max_lc]  # NT programmed only for 6 cycles!
+                    plt.axis(axis)
+                    plt.title(ch + ' for ' + problem + ' for factor=' + fs)
+                    lgd = plt.legend(bbox_to_anchor=(1.7, 1.0))
+                    plt.savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + 'lc.png', bbox_extra_artists=(lgd,),
+                                bbox_inches='tight', dpi=300)
+                plt.close()
+
+# the same plots, now splitted for meshes
+for ch in plot1:
+    if ch in characteristics_timelines:
+        for (f, fs) in factors.iteritems():
+            for problem in problems.iterkeys():
+                for i in meshes:
+                    plot_empty = True
+                    colors = plt.get_cmap('jet')(np.linspace(0, 1.0, 5))
+                    max_ = 0
+                    min_ = 1e16
+                    max_lc = 0
+                    min_lc = 1e16
+                    for (t, dt_ms) in dts.iteritems():
+                        name = problem + ('%d%d%d' % (f, i, t))
+                        x = []
+                        y = []
+                        if name in problems[problem]:
+                            d = problems[problem][name]
+                            y = d['timelines'][ch]
+                            x = times[t]
+                            newmin = min(y[dtToSteps[t]:])
+                            if newmin < min_:
+                                min_ = newmin
+                            newmax = max(y[dtToSteps[t]:])
+                            if newmax > max_:
+                                max_ = newmax
+                            newmin = min(y[(5*dtToSteps[t]):])  # NT programmed only for 6 cycles!
+                            if newmin < min_lc:
+                                min_lc = newmin
+                            newmax = max(y[(5*dtToSteps[t]):])  # NT programmed only for 6 cycles!
+                            if newmax > max_lc:
+                                max_lc = newmax
+                        if y:
+                            plot_empty = False
+                            print(ch, ' on mesh ', i)
+                            plt.plot(x, y, '-', label=problem + (' on mesh %d' % i) + 'with dt=%d ms' % dt_ms,
+                                     color=colors[t-1])
+                    if not plot_empty:
+                        plt.xlabel('time')
+                        axis = [0, 6, min_, max_]  # NT programmed only for 6 cycles!
+                        plt.axis(axis)
+                        plt.title(ch + ' for ' + problem + ' for factor=' + fs)
+                        lgd = plt.legend(bbox_to_anchor=(1.7, 1.0))
+                        plt.savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + 'm%d' % i + '.png',
+                                    bbox_extra_artists=(lgd,), bbox_inches='tight', dpi=300)
+                        axis = [5, 6, min_lc, max_lc]  # NT programmed only for 6 cycles!
+                        plt.axis(axis)
+                        plt.title(ch + ' for ' + problem + ' for factor=' + fs)
+                        lgd = plt.legend(bbox_to_anchor=(1.7, 1.0))
+                        plt.savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + 'm%d' % i + 'lc.png',
+                                    bbox_extra_artists=(lgd,), bbox_inches='tight', dpi=300)
+                    plt.close()
