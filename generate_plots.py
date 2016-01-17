9fbe65b9a63b17d97b6c2a956ec3b4c01bf6127b
j-hr <hrncir.jakub@gmail.com>
2016-01-18 00:07:34 +0100

    Minor changes.

--- generate_plots.py
+++ generate_plots.py
@@ -21,6 +21,18 @@ dts = {1: 100, 2: 50, 3: 10, 4: 5, 5: 1}
 dtToSteps = {1: 10, 2: 20, 3: 100, 4: 200, 5: 1000}  # time steps per second
 
 
+def savefig(path, lgd, **kwargs):
+    plt.savefig(path, bbox_extra_artists=(lgd,), bbox_inches='tight', dpi=dpi, **kwargs)
+
+
+def minmax(minval, maxval, newmin, newmax):
+    if newmin < minval:
+        minval = newmin
+    if newmax > maxval:
+        maxval = newmax
+    return minval, maxval
+
+
 # load general reports =========================================================================
 def load_general_reports():
     csvfile = open('done_merged.csv', 'r')
@@ -90,9 +102,12 @@ def load_timelines_data():
 # create convergence plots =========================================================================
 # default characteristics: ['CE_L2r', 'CE_H1r', 'CE_H1wr', 'PEn', 'TE_L2r', 'TE_H1r', 'TE_H1wr', 'PTEn', 'FEr']
 def create_convergence_plots():
-    characteristics = ['CE_H1r']
+    # TODO simplify code using subplots (create both convergence plots simultaneously) or by using figures as variables?
     formats3 = ['x:', '+--', '1-']
     line_widths3 = [3.0, 2.0, 1.5]
+    line_width5 = 3.0
+    marker_size = 10.0
+    marker_edge_width = 2.0
     formats5 = ['1:', '2:', '3:', 'x:', '+:']
     for ch in characteristics:
         # CHARACTERISTIC/DT
@@ -118,7 +133,7 @@ def create_convergence_plots():
                         plot_empty = False
                         print(ch, ' on mesh ', i, x, y)
                         plt.plot(x, y, formats3[i-1], label=problem + (' on mesh %d' % i), color=colors[p_number],
-                                 lw=line_widths3[i-1], ms=10.0, mew=2.0)
+                                 lw=line_widths3[i-1], ms=marker_size, mew=marker_edge_width)
                 p_number += 1
             if not plot_empty:
                 plt.xlabel('dt in ms')
@@ -130,13 +145,11 @@ def create_convergence_plots():
                 plt.axis(axis)
                 plt.title(ch + ' for factor=' + fs)
                 lgd = plt.legend(bbox_to_anchor=(1.5, 1.0))
-                plt.savefig('plots/C_' + ch + '_f%d' % f + '_CT.png', bbox_extra_artists=(lgd,), bbox_inches='tight',
-                            dpi=dpi)
+                savefig('plots/C_' + ch + '_f%d' % f + '_CT.png', lgd)
             plt.close()
         # CHARACTERISTIC/H
         for (f, fs) in factors.iteritems():
             fig = plt.figure()
-            ax = fig.add_subplot(111)
             plot_empty = True
             p_number = 0
             for problem in problems.iterkeys():
@@ -156,8 +169,8 @@ def create_convergence_plots():
                     if y and sum(y) > 0:
                         plot_empty = False
                         print(ch, ' dt: ', dt_ms, x, y)
-                        ax.plot(x, y, formats5[t-1], label=problem + (' with dt %d' % dt_ms), color=colors[p_number],
-                                 lw=3.0, ms=10.0, mew=2.0)
+                        plt.plot(x, y, formats5[t-1], label=problem + (' with dt %d' % dt_ms), color=colors[p_number],
+                                 lw=line_width5, ms=marker_size, mew=marker_edge_width)
                 p_number += 1
             if not plot_empty:
                 plt.xlabel('h')
@@ -170,50 +183,80 @@ def create_convergence_plots():
                 plt.xticks((2.0, 1.0, 0.5), ('2.0', '1.0', '0.5'))
                 plt.title(ch + ' for factor=' + fs)
                 lgd = plt.legend(bbox_to_anchor=(1.5, 1.0))
-                plt.savefig('plots/C_' + ch + '_f%d' % f + '_CS.png', bbox_extra_artists=(lgd,), bbox_inches='tight',
-                            dpi=dpi)
+                savefig('plots/C_' + ch + '_f%d' % f + '_CS.png', lgd)
             plt.close()
+    # same plots for individual problems
+    for ch in characteristics:
+        # CHARACTERISTIC/DT
+        for (f, fs) in factors.iteritems():
+            for problem in problems.iterkeys():
+                plot_empty = True
+                for i in meshes:
+                    x = []
+                    y = []
+                    for (t, dt_ms) in dts.iteritems():
+                        name = problem + ('%d%d%d' % (f, i, t))
+                        if name in problems[problem]:
+                            d = problems[problem][name]
+                            value = d['report'][ch_index[ch]]
+                            if value < 1e12:  # do not plot values from diverging problems
+                                y.append(value)
+                                x.append(dt_ms)
+                    if y and sum(y) > 0:
+                        plot_empty = False
+                        print(ch, ' on mesh ', i, x, y)
+                        plt.plot(x, y, formats3[i-1], label=problem + (' on mesh %d' % i),
+                                 lw=line_widths3[i-1], ms=marker_size, mew=marker_edge_width)
+                if not plot_empty:
+                    plt.xlabel('dt in ms')
+                    plt.xscale('log')
+                    plt.yscale('log')
+                    axis = plt.axis()
+                    # print(axis)
+                    axis = [105, 0.95, axis[2], axis[3]]
+                    plt.axis(axis)
+                    plt.title(ch + ' for factor=' + fs)
+                    lgd = plt.legend(bbox_to_anchor=(1.5, 1.0))
+                    savefig('plots/C_' + ch + '_f%d' % f + problem + '_CT.png', lgd)
+                plt.close()
+        # CHARACTERISTIC/H
+        for (f, fs) in factors.iteritems():
+            for problem in problems.iterkeys():
+                plot_empty = True
+                for (t, dt_ms) in dts.iteritems():
+                    x = []
+                    y = []
+                    for i in meshes:
+                        name = problem + ('%d%d%d' % (f, i, t))
+                        if name in problems[problem]:
+                            d = problems[problem][name]
+                            value = d['report'][ch_index[ch]]
+                            if value < 1e12:  # do not plot values from diverging problems
+                                y.append(value)
+                                x.append(d['md']['h'])
+                    if y and sum(y) > 0:
+                        plot_empty = False
+                        print(ch, ' dt: ', dt_ms, x, y)
+                        plt.plot(x, y, formats5[t-1], label=problem + (' with dt %d' % dt_ms),
+                                 lw=line_width5, ms=marker_size, mew=marker_edge_width)
+                if not plot_empty:
+                    plt.xlabel('h')
+                    plt.xscale('log')
+                    plt.yscale('log')
+                    axis = plt.axis()
+                    # print(axis)
+                    axis = [2.3, 0.5, axis[2], axis[3]]
+                    plt.axis(axis)
+                    plt.xticks((2.0, 1.0, 0.5), ('2.0', '1.0', '0.5'))
+                    plt.title(ch + ' for factor=' + fs)
+                    lgd = plt.legend(bbox_to_anchor=(1.5, 1.0))
+                    savefig('plots/C_' + ch + '_f%d' % f + problem + '_CS.png', lgd)
+                plt.close()
 
 
 def create_timelines_plots():
-    # last time characteristics for timelines:
-    # 'AVN_L2', 'AVN_H1',
-    # 'CE_L2', 'CE_L2s', 'CE_L2n', 'CE_L2r',
-    # 'DT', 'DTs',
-    # 'APG', 'APGs', 'APGn',
-    # 'PGEA', 'PGEAs', 'PGEAn',
-    # 'CE_H1', 'CE_H1s', 'CE_H1r',
-    # 'PG', 'PGs', 'PGn',
-    # 'AF',
-    # 'CE_H1w', 'CE_H1ws', 'CE_H1wr',
-    # 'APN',
-    # 'TE_L2', 'TE_L2s', 'TE_L2n', 'TE_L2r',
-    # 'TE_H1', 'TE_H1s', 'TE_H1r',
-    # 'DC', 'DCs',
-    # 'TE_H1w', 'TE_H1ws', 'TE_H1wr',
-    # 'PE', 'PEs', 'PEn',
-    # 'PGE', 'PGEs', 'PGEn',
-    # 'FE', 'FEr',
-    # 'AVN_H1w'
-
-    # define plots:
-
-    # plot: one parameter, one problem, one factor, 3 meshes, 5 dts
-    plot1 = ['CE_H1r']
-    # plot1 = ['CE_H1r', 'CE_H1wr', 'FEr']
-    # plot1 = ['CE_L2', 'CE_L2r', 'CE_H1', 'CE_H1r', 'CE_H1w', 'CE_H1wr', 'TE_L2', 'TE_L2r', 'TE_H1', 'TE_H1t', 'TE_H1w',
-    #          'TE_H1wr', 'FE', 'FEr', 'PE', 'PEn', 'PGE', 'PGEn',]
-
-    # QQ which to use?  >> let choose shorter set
-
-    # same plot with analytic value
-    plot2 = {'PG': 'APG'}  # NT: iplement ad hoc
-
-    # QQ other plots?
-
     formats3 = ['-', '--', ':']
     line_widths3 = [2.0, 1.5, 1.0]
-
     for ch in plot1:
         if ch in characteristics_timelines:
             print(ch)
@@ -225,8 +268,8 @@ def create_timelines_plots():
                     colors = plt.get_cmap('jet')(np.linspace(0, 1.0, 5))
                     max_ = 0
                     min_ = 1e16
-                    max_lc = 0
-                    min_lc = 1e16
+                    max_l = 0
+                    min_l = 1e16
                     for i in meshes:
                         for (t, dt_ms) in dts.iteritems():
                             name = problem + ('%d%d%d' % (f, i, t))
@@ -237,21 +280,11 @@ def create_timelines_plots():
                                 y = d['timelines'][ch]
                                 x = times[t]
                                 print('      ' + name + ' data sizes (x, y):', len(x), len(y), 'expected', 6*dtToSteps[t])
-                                newmin = min(y[dtToSteps[t]:])
-                                if newmin < min_:
-                                    min_ = newmin
-                                newmax = max(y[dtToSteps[t]:])
-                                if newmax > max_:
-                                    max_ = newmax
-                                newmin = min(y[(5*dtToSteps[t]):])  # NT programmed only for 6 cycles!
-                                if newmin < min_lc:
-                                    min_lc = newmin
-                                newmax = max(y[(5*dtToSteps[t]):])  # NT programmed only for 6 cycles!
-                                if newmax > max_lc:
-                                    max_lc = newmax
+                                min_, max_ = minmax(min_, max_, min(y[dtToSteps[t]:]), max(y[dtToSteps[t]:]))
+                                min_l, max_l = minmax(min_l, max_l, min(y[5*dtToSteps[t]:]), max(y[5*dtToSteps[t]:]))
+                                # NT programmed only for 6 cycles!
                             if y:
                                 plot_empty = False
-                                print('        ', ch, ' on mesh ', i)
                                 plt.plot(x, y, formats3[i-1], label=problem + (' on mesh %d' % i) + 'with dt=%d ms' % dt_ms,
                                          color=colors[t-1], lw=line_widths3[i-1])
                     if not plot_empty:
@@ -262,29 +295,30 @@ def create_timelines_plots():
                         plt.axis(axis)
                         plt.title(ch + ' for ' + problem + ' for factor=' + fs)
                         lgd = plt.legend(bbox_to_anchor=(1.7, 1.0))
-                        plt.savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + '.png', bbox_extra_artists=(lgd,),
-                                    bbox_inches='tight', dpi=dpi)
+                        savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + '.png', lgd)
                         # save same plot only for last cycle
-                        axis = [5, 6, min_lc, max_lc]  # NT programmed only for 6 cycles!
+                        axis = [5, 6, min_l, max_l]  # NT programmed only for 6 cycles!
                         plt.axis(axis)
                         plt.title(ch + ' for ' + problem + ' for factor=' + fs)
                         lgd = plt.legend(bbox_to_anchor=(1.7, 1.0))
-                        plt.savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + 'lc.png', bbox_extra_artists=(lgd,),
-                                    bbox_inches='tight', dpi=dpi)
+                        savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + 'lc.png', lgd)
                     plt.close()
 
     # the same plots, now splitted for meshes
     for ch in plot1:
         if ch in characteristics_timelines:
+            print(ch)
             for (f, fs) in factors.iteritems():
+                print('  f =', fs)
                 for problem in problems.iterkeys():
+                    print('    problem =', problem)
                     for i in meshes:
                         plot_empty = True
                         colors = plt.get_cmap('jet')(np.linspace(0, 1.0, 5))
                         max_ = 0
                         min_ = 1e16
-                        max_lc = 0
-                        min_lc = 1e16
+                        max_l = 0
+                        min_l = 1e16
                         for (t, dt_ms) in dts.iteritems():
                             name = problem + ('%d%d%d' % (f, i, t))
                             x = []
@@ -293,21 +327,12 @@ def create_timelines_plots():
                                 d = problems[problem][name]
                                 y = d['timelines'][ch]
                                 x = times[t]
-                                newmin = min(y[dtToSteps[t]:])
-                                if newmin < min_:
-                                    min_ = newmin
-                                newmax = max(y[dtToSteps[t]:])
-                                if newmax > max_:
-                                    max_ = newmax
-                                newmin = min(y[(5*dtToSteps[t]):])  # NT programmed only for 6 cycles!
-                                if newmin < min_lc:
-                                    min_lc = newmin
-                                newmax = max(y[(5*dtToSteps[t]):])  # NT programmed only for 6 cycles!
-                                if newmax > max_lc:
-                                    max_lc = newmax
+                                print('      ' + name + ' data sizes (x, y):', len(x), len(y), 'expected', 6*dtToSteps[t])
+                                min_, max_ = minmax(min_, max_, min(y[dtToSteps[t]:]), max(y[dtToSteps[t]:]))
+                                min_l, max_l = minmax(min_l, max_l, min(y[5*dtToSteps[t]:]), max(y[5*dtToSteps[t]:]))
+                                # NT programmed only for 6 cycles!
                             if y:
                                 plot_empty = False
-                                print(ch, ' on mesh ', i)
                                 plt.plot(x, y, '-', label=problem + (' on mesh %d' % i) + 'with dt=%d ms' % dt_ms,
                                          color=colors[t-1], lw=1.0)
                         if not plot_empty:
@@ -318,14 +343,12 @@ def create_timelines_plots():
                             plt.axis(axis)
                             plt.title(ch + ' for ' + problem + ' for factor=' + fs)
                             lgd = plt.legend(bbox_to_anchor=(1.7, 1.0))
-                            plt.savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + 'm%d' % i + '.png',
-                                        bbox_extra_artists=(lgd,), bbox_inches='tight', dpi=dpi)
-                            axis = [5, 6, min_lc, max_lc]  # NT programmed only for 6 cycles!
+                            savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + 'm%d' % i + '.png', lgd)
+                            axis = [5, 6, min_l, max_l]  # NT programmed only for 6 cycles!
                             plt.axis(axis)
                             plt.title(ch + ' for ' + problem + ' for factor=' + fs)
                             lgd = plt.legend(bbox_to_anchor=(1.7, 1.0))
-                            plt.savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + 'm%d' % i + 'lc.png',
-                                        bbox_extra_artists=(lgd,), bbox_inches='tight', dpi=dpi)
+                            savefig('plots/TL1_' + problem + '_' + ch + '_f%d' % f + 'm%d' % i + 'lc.png', lgd)
                         plt.close()
 
 
@@ -342,7 +365,38 @@ if not os.path.exists('plots'):
 load_general_reports()
 load_seconds_data()
 load_timelines_data()
+characteristics = ['CE_H1r']
 create_convergence_plots()
+# last time characteristics for timelines:
+# 'AVN_L2', 'AVN_H1',
+# 'CE_L2', 'CE_L2s', 'CE_L2n', 'CE_L2r',
+# 'DT', 'DTs',
+# 'APG', 'APGs', 'APGn',
+# 'PGEA', 'PGEAs', 'PGEAn',
+# 'CE_H1', 'CE_H1s', 'CE_H1r',
+# 'PG', 'PGs', 'PGn',
+# 'AF',
+# 'CE_H1w', 'CE_H1ws', 'CE_H1wr',
+# 'APN',
+# 'TE_L2', 'TE_L2s', 'TE_L2n', 'TE_L2r',
+# 'TE_H1', 'TE_H1s', 'TE_H1r',
+# 'DC', 'DCs',
+# 'TE_H1w', 'TE_H1ws', 'TE_H1wr',
+# 'PE', 'PEs', 'PEn',
+# 'PGE', 'PGEs', 'PGEn',
+# 'FE', 'FEr',
+# 'AVN_H1w'
+
+# define plots:
+# plot: one parameter, one problem, one factor, 3 meshes, 5 dts
+plot1 = ['CE_H1r']
+# plot1 = ['CE_H1r', 'CE_H1wr', 'FEr']
+# plot1 = ['CE_L2', 'CE_L2r', 'CE_H1', 'CE_H1r', 'CE_H1w', 'CE_H1wr', 'TE_L2', 'TE_L2r', 'TE_H1', 'TE_H1t', 'TE_H1w',
+#          'TE_H1wr', 'FE', 'FEr', 'PE', 'PEn', 'PGE', 'PGEn',]
+# QQ which to use?  >> let choose shorter set
+# same plot with analytic value
+plot2 = {'PG': 'APG'}  # NT: iplement ad hoc, does nothing
+# QQ other plots?
 create_timelines_plots()
 
 
@@ -374,6 +428,6 @@ create_timelines_plots()
 #         plt.xticks(seconds_list)
 #         lgd = plt.legend(bbox_to_anchor=(1.4, 1.0))
 #         plt.axis([min(seconds_list), max(seconds_list), 0, max_value])
-#         plt.savefig('plots/S_' + type_key + '.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
+#         savefig('plots/S_' + type_key + '.png', lgd)
 #     plt.close()
 
